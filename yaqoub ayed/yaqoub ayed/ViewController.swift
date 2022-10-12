5f069533d0f00147d68f5392b3023dd7098a51eb
yaqoub06 <114612080+yaqoub06@users.noreply.github.com>
2022-10-12 19:11:15 +0300

    y

---
+++ yaqoub ayed/yaqoub ayed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yaqoub ayed
+//
+//  Created by yaqoub ayed salman on 10/12/22.
+//  Copyright © 2022 yaqoub ayed salman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
